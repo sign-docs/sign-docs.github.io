b3e439f9374af898630482e6d0863b716a33070b
Stan Chen <stanchensz@gmail.com>
2025-09-24 22:46:17 -0600

    Switch to Tailwind via PostCSS, import index.css, remove CDN

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
